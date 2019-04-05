26d3b62548d4c9e9dd902f8344f13e604b74ce62
Amir Sohi <amirasohi@gmail.com>
2019-04-05 19:01:11 +0100

    Implemented selectors to project

--- src/HomeScreen/HomeScreenHoc.js
+++ src/HomeScreen/HomeScreenHoc.js
@@ -1,10 +1,12 @@
 import { logWeight } from "../hoc/containers/UserData/actions";
+import { makeSelectUserWeight } from "../hoc/containers/UserData/selectors";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-const mapStateToProps = state => {
-  const userWeight = state.userData.userWeight;
-  return { userWeight };
-};
+// the createStructuredSelector means we can use the selector when mapping our state to props
+const mapStateToProps = createStructuredSelector({
+  userWeight: makeSelectUserWeight()
+});
 
 const mapDispatchToProps = {
   logWeight
